3fbc50a120521e40a02525242b2964a0fcb53d25
Gautam Giri <gautamgirimantracare@gmail.com>
2025-02-05 09:31:44 +0530

    updated newday.sh and run.sh and created day7

--- newday.sh
+++ newday.sh
@@ -1,1 +1,18 @@
-cp -r template day$1
+#!/bin/bash
+
+# Get the last folder number (assumes folders are named 'day1', 'day2', etc.)
+last_day=$(ls -d "day"* | grep -o '[0-9]*' | sort -n | tail -n 1)
+
+# If no 'day' folder is found, start with day1
+if [ -z "$last_day" ]; then
+    next_day=1
+else
+    # Increment the last folder number to create the next folder
+    next_day=$((last_day + 1))
+fi
+
+# Create the new folder
+new_folder="day$next_day"
+cp -r template $new_folder
+
+echo "Created folder: $new_folder"
--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
+#!/bin/bash
 node day$1/puzzle$2.js day$1/$3.txt --inspect
