898238131e72a67d7baff27b6968db65a321c114
Gautam Giri <personal.gautamgiri@gmail.com>
2025-02-05 11:03:16 +0000

    solved day 8

--- day8/puzzle2.js
+++ day8/puzzle2.js
@@ -1,7 +1,7 @@
 const fs = require("node:fs");
 
-function isInBounds(rows, cols, row, col) {
-  return row >= 0 && col >= 0 && row < rows && col < cols;
+function isInBounds(height, width, row, col) {
+  return row >= 0 && col >= 0 && row < height && col < width;
 }
 
 function getLocationDiff(location1, location2, opposite = false) {
@@ -10,25 +10,32 @@ function getLocationDiff(location1, location2, opposite = false) {
   else return [location1[0] - location2[0], location1[1] - location2[1]];
 }
 
-function findPossibleAntinode(location1, location2, diff) {
+function findPossibleAntinode(location, diff, verify, height, width) {
   // one of the antinodes is actually antenna location...
-  const possibleAntinode1 = getLocationDiff(location1, diff, true);
-  const possibleAntinode2 = getLocationDiff(location1, diff);
-
-  return possibleAntinode1[0] != location2[0]
-    ? possibleAntinode1
-    : possibleAntinode2;
+  let possibleAntinode1 = location;
+  do {
+    possibleAntinode1 = getLocationDiff(possibleAntinode1, diff, true);
+    verify(possibleAntinode1);
+  } while (
+    isInBounds(height, width, possibleAntinode1[0], possibleAntinode1[1])
+  );
+  let possibleAntinode2 = location;
+  do {
+    possibleAntinode2 = getLocationDiff(possibleAntinode2, diff);
+    verify(possibleAntinode2);
+  } while (
+    isInBounds(height, width, possibleAntinode2[0], possibleAntinode2[1])
+  );
 }
 
-function getAntinodes(frequencyLocations, verify) {
+function getAntinodes(height, width, frequencyLocations, verify) {
   for (let i = 0; i < frequencyLocations.length; i++) {
     const location1 = frequencyLocations[i];
     for (let j = i + 1; j < frequencyLocations.length; j++) {
       const location2 = frequencyLocations[j];
       const diff = getLocationDiff(location1, location2);
-      const antinode1 = findPossibleAntinode(location1, location2, diff);
-      const antinode2 = findPossibleAntinode(location2, location1, diff);
-      verify(antinode1, antinode2);
+      findPossibleAntinode(location1, diff, verify, height, width);
+      findPossibleAntinode(location2, diff, verify, height, width);
     }
   }
 }
@@ -36,7 +43,7 @@ function getAntinodes(frequencyLocations, verify) {
 function solvePuzzle(mappedArea) {
   const frequencyLocations = new Map();
   const visitedAntinodes = new Set();
-  let ans = 0;
+  const [height, width] = [mappedArea.length, mappedArea[0].length];
 
   for (let i = 0; i < mappedArea.length; i++) {
     for (let j = 0; j < mappedArea[i].length; j++) {
@@ -50,16 +57,9 @@ function solvePuzzle(mappedArea) {
   }
 
   for (const locations of frequencyLocations.values()) {
-    getAntinodes(locations, (...antinodes) => {
+    getAntinodes(height, width, locations, (...antinodes) => {
       antinodes.forEach((antinode) => {
-        if (
-          isInBounds(
-            mappedArea.length,
-            mappedArea[0].length,
-            antinode[0],
-            antinode[1]
-          )
-        ) {
+        if (isInBounds(height, width, antinode[0], antinode[1])) {
           const actualVal = mappedArea[antinode[0]][antinode[1]];
           visitedAntinodes.add(antinode.toString());
           if (actualVal == ".") {
